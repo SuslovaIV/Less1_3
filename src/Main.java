f1b7d1ac2059d4ba9b35e4450fcbc429217505aa
TeenGale <isis86@yandex.ru>
2022-11-10 17:41:54 +0300

    commit1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Person> per30;
+        List<Person> persons = new ArrayList<Person>();
+        persons.add(new Person(10, "Петя"));
+        persons.add(new Person(15, "Лена"));
+        persons.add(new Person(17, "Маша"));
+        persons.add(new Person(50, "Витя"));
+        persons.add(new Person(35, "Женя"));
+        persons.add(new Person(21, "Олег"));
+        persons.add(new Person(18, "Нина"));
+        persons.add(new Person(48, "Рома"));
+
+        persons.stream().forEach((person) -> System.out.println(person.getName() + ", " + person.getAge() + " лет"));
+        System.out.println("Возраст");
+        List ages = persons.stream().map((person -> person.getAge())).collect(Collectors.toList());
+        ages.stream().forEach(age -> System.out.print(age + " "));
+        System.out.println();
+        System.out.println("Имена");
+        List names = persons.stream().map((person -> person.getName())).collect(Collectors.toList());
+        names.stream().forEach(name -> System.out.print(name + " "));
+        System.out.println();
+        System.out.println("Страше 30ти:");
+        per30 = persons.stream().filter((person -> person.getAge() > 30)).collect(Collectors.toList());
+        per30.stream().forEach(p -> System.out.println(p.getName()));
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Person {
+public class Person{
+    private final int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    private final String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
 }
